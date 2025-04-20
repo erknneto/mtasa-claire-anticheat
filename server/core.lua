697a906082e1d4d3ced28f9252113427b43155d0
Ernani Sá <41498264+erknneto@users.noreply.github.com>
2025-04-20 14:04:15 -0300

    Removed update check

--- server/core.lua
+++ server/core.lua
@@ -13,22 +13,3 @@ addEvent("clairePunish", true)
 addEventHandler("clairePunish", root, function(reason)
     clairePunish(client, reason)
 end)
-
-setTimer(function()
-	fetchRemote("https://raw.githubusercontent.com/erknneto/mtasa-claire-anticheat/refs/heads/main/meta.xml", function(responseCode, data)
-		if responseCode == 200 then
-			local remoteVersion = string.match(data, 'version="(.-)"')
-			local localMeta = xmlLoadFile("meta.xml")
-			local localVersion = localMeta and xmlNodeGetAttribute(xmlFindChild(localMeta, "info", 0), "version") or "unknown"
-			if localMeta then xmlUnloadFile(localMeta) end
-
-			if remoteVersion and localVersion and remoteVersion ~= localVersion then
-				outputDebugString("[Claire] Update available! Local version: " .. tostring(localVersion) .. " | Latest: " .. remoteVersion, 3)
-				outputDebugString("[Claire] Download latest at: https://github.com/erknneto/mtasa-claire-anticheat", 3)
-				
-				outputServerlog("[Claire] Update available! Local version: " .. tostring(localVersion) .. " | Latest: " .. remoteVersion, 3)
-				outputServerlog("[Claire] Download latest at: https://github.com/erknneto/mtasa-claire-anticheat", 3)
-			end
-		end
-	end, "", false)
-end, 3600000, 0)
